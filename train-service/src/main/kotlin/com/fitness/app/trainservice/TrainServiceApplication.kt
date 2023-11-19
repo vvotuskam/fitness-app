15665f4885cb0072416f754af8f5e10949685ee6
Asulan Maksut <a.maksut@strongte.am>
2023-11-19 22:27:53 +0600

    init train-service

---
+++ train-service/src/main/kotlin/com/fitness/app/trainservice/TrainServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fitness.app.trainservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrainServiceApplication>(*args)
+}
---
+++ train-service/src/test/kotlin/com/fitness/app/trainservice/TrainServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fitness.app.trainservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
